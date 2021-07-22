8b321d3c2b547d9a2789f26eec628b0af5e503f2
zefty <jaime.wu011@gmail.com>
2021-07-22 14:03:09 +1200

    fix path

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ if (!isDev && cluster.isMaster) {
   app.use(bodyParser.json({ extended: true }));
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.join(__dirname, '/react-ui/build')));
 
   // Answer API requests.
   app.get('/api', function (req, res) {
