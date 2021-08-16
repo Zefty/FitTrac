914c0ac2775d636d1b3f955fcc302a94269818ab
zefty <jaime.wu011@gmail.com>
2021-08-16 17:06:24 +1200

    update env vars

--- react-ui/src/api/firebase.tsx
+++ react-ui/src/api/firebase.tsx
@@ -6,13 +6,13 @@ import { firebasecontext } from '../types/types';
 try {
     console.info("Initializing Firebase Connection ...")
     firebase.initializeApp({
-        apiKey: process.env.APIKEY,
-        authDomain: process.env.AUTHDOMAIN,
-        projectId: process.env.PROJECTID,
-        storageBucket: process.env.STORAGEBUCKET,
-        messagingSenderId: process.env.MESSAGINGSENDERID,
-        appId: process.env.APPID,
-        measurementId: process.env.MEASUREMENTID
+        apiKey: process.env.REACT_APP_APIKEY,
+        authDomain: process.env.REACT_APP_AUTHDOMAIN,
+        projectId: process.env.REACT_APP_PROJECTID,
+        storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+        appId: process.env.REACT_APP_APPID,
+        measurementId: process.env.REACT_APP_MEASUREMENTID
     });
 } catch (err) {
     if (!/already exists/.test(err.message)) {
